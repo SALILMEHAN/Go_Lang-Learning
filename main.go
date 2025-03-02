078587e6f89edb094c0459ae723cdb55bca291c0
Salil Mehan <salilmehan123@gmail.com>
2025-03-02 20:32:25 +0530

    video 7

--- main.go
+++ main.go
@@ -15,4 +15,29 @@ func main() {
 
 	// Video 6:------------------------------------------------------------
 	any_name.PrintName("Salil")
+
+	// Video 7:------------------------------------------------------------
+	var name string = "yy" //declating like this shows the variable is 100% string
+	var version = "ee"     //declaring like this, go lang compiler automatically detects the type of variable declared
+	fmt.Println(name)
+	fmt.Println(version)
+
+	var money = 1234
+	fmt.Println("Price:", money)
+
+	var decided = true
+	fmt.Println(decided)
+
+	const pi = 3.14
+	// pi = 3		//it will show error
+	fmt.Println(pi)
+
+	//This is important, for declating the variable most developers use like below
+	myName := "Salil"
+	fmt.Println(myName)
+
+	var PublicVariable = 123 //when we initialize the variable name with 1st letter capital then that variable is exportable for other files. Ex:- Println() function
+	var privateVariable = 456
+	fmt.Println(PublicVariable)
+	fmt.Println(privateVariable)
 }
