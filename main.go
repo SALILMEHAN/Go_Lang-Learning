000d8900a25807c01dbc7ae01e0fa48ee1fac189
Salil Mehan <salilmehan123@gmail.com>
2025-03-02 21:01:45 +0530

    video 8

--- main.go
+++ main.go
@@ -1,43 +1,51 @@
 package main
 
-import (
-	"abc/any_name"
-	"fmt"
-)
+import "fmt"
 
 // https://www.youtube.com/playlist?list=PLzjZaW71kMwSEVpdbHPr0nPo5zdzbDulm
 
 func main() {
 	// Video 5:------------------------------------------------------------
-	// go mod init abc
+	// // go mod init abc
 	fmt.Println("Hi!")
-	// go run main.go ---->> execute this on terminal
+	// // go run main.go ---->> execute this on terminal
 
-	// Video 6:------------------------------------------------------------
-	any_name.PrintName("Salil")
+	// Video 6 (Importing Packages):------------------------------------------------------------
+	// any_name.PrintName("Salil")
 
-	// Video 7:------------------------------------------------------------
-	var name string = "yy" //declating like this shows the variable is 100% string
-	var version = "ee"     //declaring like this, go lang compiler automatically detects the type of variable declared
-	fmt.Println(name)
-	fmt.Println(version)
+	// Video 7 (Variables):------------------------------------------------------------
+	// var name string = "yy" //declating like this shows the variable is 100% string
+	// var version = "ee"     //declaring like this, go lang compiler automatically detects the type of variable declared
+	// fmt.Println(name)
+	// fmt.Println(version)
 
-	var money = 1234
-	fmt.Println("Price:", money)
+	// var money = 1234
+	// fmt.Println("Price:", money)
 
-	var decided = true
-	fmt.Println(decided)
+	// var decided = true
+	// fmt.Println(decided)
 
-	const pi = 3.14
-	// pi = 3		//it will show error
-	fmt.Println(pi)
+	// const pi = 3.14
+	// // pi = 3		//it will show error
+	// fmt.Println(pi)
 
-	//This is important, for declaring the variable most developers use like below
-	myName := "Salil"
-	fmt.Println(myName)
+	// //This is important, for declaring the variable most developers use like below
+	// myName := "Salil"
+	// fmt.Println(myName)
 
-	var PublicVariable = 123 //when we initialize the variable name with 1st letter capital then that variable is exportable for other files. Ex:- Println() function
-	var privateVariable = 456
-	fmt.Println(PublicVariable)
-	fmt.Println(privateVariable)
+	// var PublicVariable = 123 //when we initialize the variable name with 1st letter capital then that variable is exportable for other files. Ex:- Println() function
+	// var privateVariable = 456
+	// fmt.Println(PublicVariable)
+	// fmt.Println(privateVariable)
+
+	// Video 8 (println and printf in Golang):------------------------------------------------------------
+	age := 22
+	name := "ww"
+	height := 5.6794838
+	fmt.Println("Name:", name, "Age:", age, "Height:", height)
+
+	fmt.Printf("My height is %.3f", height)
+	fmt.Printf("My age is %d\n", age)
+	fmt.Printf("My name is %s\n", name)
+	fmt.Printf("Name %s, Age %d, Height %.3f", name, age, height)
 }
