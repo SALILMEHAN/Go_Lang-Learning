e9f918a46a2099df5d418b57c79149b3679dcba9
Salil Mehan <salilmehan123@gmail.com>
2025-03-02 20:09:53 +0530

    first commit

---
+++ any_name/any_name.go
@@ -0,0 +1,8 @@
+package any_name
+
+import "fmt"
+
+// Exported function (starts with uppercase)
+func PrintName(message string) {
+	fmt.Println(message)
+}
---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"abc/any_name"
+	"fmt"
+)
+
+// https://www.youtube.com/playlist?list=PLzjZaW71kMwSEVpdbHPr0nPo5zdzbDulm
+
+func main() {
+	// Video 5:------------------------------------------------------------
+	// go mod init abc
+	fmt.Println("Hi!")
+	// go run main.go ---->> execute this on terminal
+
+	// Video 6:------------------------------------------------------------
+	any_name.PrintName("Salil")
+}
